76a5f8608c289c59e03c76ec8de15f60324b8be9
Nikoline Christensen <nich1003@stud.ek.dk>
2026-04-17 13:23:29 +0200

    first commit forms

--- js/main.js
+++ js/main.js
@@ -27,3 +27,32 @@ function infobrun() {
   document.querySelector(".info-subheadline").textContent = "Brune blade betyder ofte, at planten er stresset eller udtørret";
   document.querySelector("#efficiency").innerHTML = "<h3>hvorfor er den brun?</h3> <p> det er den fordi....</p>";
 }
+
+const outputName = document.querySelector("#name_output");
+const outputEmail = document.querySelector("#email_output");
+const outputDepartment = document.querySelector("#department_output");
+const outputMessage = document.querySelector("#message_output");
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  outputName.textContent = formData.get("full_name");
+  outputEmail.textContent = formData.get("email");
+  // 1. saml værdierne fra select og textarea og vis dem i output-sektionen
+  const department = formData.get("department");
+  outputDepartment.textContent = department;
+  outputMessage.textContent = formData.get("message");
+
+  form.reset();
+}
+
+form.addEventListener("submit", handleSubmit);
+const form = document.querySelector("form");
+
+function cancelPopup(event) {
+  event.preventDefault();
+  form.querySelector(":user-invalid").focus();
+}
+
+form.addEventListener("invalid", cancelPopup, true);
